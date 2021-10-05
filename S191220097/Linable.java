78fa020f8d32dd44d597c80d22094caa9a5eee10
Cindy <1364195528@qq.com>
2021-10-05 17:20:32 +0800

    first commit

---
+++ S191220097/Linable.java
@@ -0,0 +1,13 @@
+package S191220097;
+
+import S191220097.Line.Position;
+
+public interface Linable {
+
+    public void setPosition(Position position);
+
+    public Position getPosition();
+
+    public int getValue();
+
+}
---
+++ S191220097/Sorter.java
@@ -0,0 +1,11 @@
+package S191220097;
+
+public interface Sorter {
+
+    public void load(int[] elements);
+
+    public void sort();
+
+    public String getPlan();
+
+}
